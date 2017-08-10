b94a3d0d06028fa467ff1966adda9c914b917ad2
yinizhizhu <lijiahe19941013@sina.cn>
2017-08-10 15:15:07 +0800

    update

--- code/rotate.m
+++ code/rotate.m
@@ -1,15 +1,6 @@
 function rotate()
-T = zeros(3, 3);
 
-theta = pi/6;
-T(1, 1) = cos(theta);
-T(2, 2) = cos(theta);
-T(1, 2) = sin(theta);
-T(2, 1) = -sin(theta);
-T(3, 3) = 1;
-T = T^-1;
-
-f = 255-imread('hit.jpg');
+[T, f] = initial();
 [row, col] = size(f);
 subplot(2, 2, 1);
 imshow(f);
@@ -17,30 +8,34 @@ imshow(f);
 centerX = floor(row/2);
 centerY = floor(col/2);
 
-g = zeros(row, col, 'uint8');
+g = zeros(row, col);
+g = g-1;
 whos g;
 for i=1:1:row
     for j=1:1:col
-        a = [i-centerX j-centerY 1]*T;
-        m = floor(a(1))+centerX;
-        n = floor(a(2))+centerY;
-        if m > 0 && m <= row
-            if n > 0 && n <= col
-                g(i, j) = f(m, n);
-            end
+        [m ,n] = getP(T, i, j, centerX, centerY);
+        pixel = getV(f, row, col, m, n);
+        if pixel >= 0
+            g(i, j) = pixel;
+        end
+    end
+end
+for i=1:1:row
+    for j=1:1:col
+        if g(i, j) < 0
+%             give the pixel with the nearest pixel
         end
     end
 end
 subplot(2, 2, 2);
 imshow(g);
 
-h = zeros(row, col, 'uint8');
+h = zeros(row, col);
+h = h-1;
 whos h
 for i=1:1:row
     for j=1:1:col
-        a = [i-centerX j-centerY 1]*T;
-        m = floor(a(1))+centerX;
-        n = floor(a(2))+centerY;
+        [m ,n] = getP(T, i, j, centerX, centerY);
         k = 0;
         total = 0;
         [pixel, step] = getV(f, row, col, m, n);
@@ -69,13 +64,29 @@ disp Done!!
 
 end
 
-function [pixel, step] = getV(f, row, col, m, n)
-pixel = 0;
-step = 0;
+function [T, f] = initial()
+T = zeros(3, 3);
+theta = pi/6;
+T(1, 1) = cos(theta);
+T(2, 2) = cos(theta);
+T(1, 2) = sin(theta);
+T(2, 1) = -sin(theta);
+T(3, 3) = 1;
+T = T^-1;
+f = 255-imread('hit.jpg');
+end
+
+function [m, n] = getP(T, i, j, centerX, centerY)
+a = [i-centerX j-centerY 1]*T;
+m = floor(a(1))+centerX;
+n = floor(a(2))+centerY;
+end
+
+function pixel = getV(f, row, col, m, n)
+pixel = -1;
 if m > 0 && m <= row
     if n > 0 && n <= col
         pixel = f(m, n);
-        step = 1;
     end
 end
 end
