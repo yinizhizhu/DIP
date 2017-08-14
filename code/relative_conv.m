80706d4703b553382187d17da66b7aa76ddcafdb
yinizhizhu <lijiahe19941013@sina.cn>
2017-08-14 17:13:06 +0800

    update

--- code/relative_conv.m
+++ code/relative_conv.m
@@ -1,15 +1,25 @@
-f = uint8(ans);
-subplot(1,3,1);
-imshow(f);
-w = zeros(3, 3, 'uint8');
+f = zeros(16,16);
 step = 1;
-for i=1:1:3
-    for j=1:1:3
-        w(i, j) = step;
+for i=1:1:16
+    for j=1:1:16
+        f(i, j) = step;
         step = step+1;
     end
 end
-w = w-5;
+subplot(1,3,1);
+imshow(uint8(f));
+
+w = zeros(3, 3);
+step = 1;
+res = 0;
+for i=1:1:3
+    for j=1:1:3
+        w(i, j) = step;
+        res = res + step;
+        step = step+1;
+    end
+end
+w = w/res;
 
 g = f;
 for i=1:1:16
@@ -32,7 +42,7 @@ for i=1:1:16
     end
 end
 subplot(1,3,2);
-imshow(g);
+imshow(uint8(g));
 
 g2 = f;
 for i=1:1:16
@@ -55,4 +65,4 @@ for i=1:1:16
     end
 end
 subplot(1,3,3);
-imshow(g2);
+imshow(uint8(g2));
